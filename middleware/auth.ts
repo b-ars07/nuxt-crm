e6d1e2d082a8b7a088f89d636ff34e01c7c1bf0c
Arsenii Berezin <b-ars07@yandex.ru>
2024-11-04 23:48:05 +0300

    инициализация структуры проекта

---
+++ middleware/auth.ts
@@ -0,0 +1,9 @@
+import {useAuthStore} from "~/stores/authStore";
+
+export default defineNuxtRouteMiddleware((to, from) => {
+    const authStore = useAuthStore()
+
+    if (!authStore.token && to.path !== '/login') {
+        return navigateTo('/login')
+    }
+})
